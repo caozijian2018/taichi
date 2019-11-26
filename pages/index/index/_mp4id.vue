7ddb3ca8a73ec49846bc16df7aa5aadc5539e781
caoyao <caoyao@testbird.com>
2019-11-26 15:21:52 +0800

    remove desc

--- pages/index/index/_mp4id.vue
+++ pages/index/index/_mp4id.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="width_100">
         <mp4 class="width_100" :mp4-src="mp4_src"></mp4>
-        <div class="font_size_15 width_90 margin_auto margin_top_10 margin_bottom_15" style="text-indent: 20px">
+        <!-- <div class="font_size_15 width_90 margin_auto margin_top_10 margin_bottom_15" style="text-indent: 20px">
             {{desc}}
-        </div>
+        </div> -->
         <video-footer></video-footer>
     </div>
 </template>
